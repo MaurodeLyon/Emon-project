fcb07db98ae54c49a7856cb2058e96f4990f3c98
Mauro de Lyon <maurodelyon@gmail.com>
2017-05-02 19:58:00 +0200

    correct time update

--- angularApplication/src/app/currentRawValues/currentRawValues.component.ts
+++ angularApplication/src/app/currentRawValues/currentRawValues.component.ts
@@ -14,7 +14,7 @@ export class CurrentRawValuesComponent {
 
   constructor(private postsService: PostsService) {
     this.postsService.getArthurMeasurements().subscribe(posts => {
-      this.arthurLineChart = new RawValuesLineChart('rgba(153, 210, 246,0.2)','rgba(153, 210, 246,1)');
+      this.arthurLineChart = new RawValuesLineChart('rgba(153, 210, 246,0.2)', 'rgba(153, 210, 246,1)');
       this.arthurLineChart.lineChartLabels = [];
       this.arthurLineChart.lineChartData = [
         {label: 'Raw tick data per hour', data: []}
@@ -28,14 +28,14 @@ export class CurrentRawValuesComponent {
       }
     });
     this.postsService.getMauroMeasurements().subscribe(posts => {
-      this.mauroLineChart = new RawValuesLineChart('rgba(66,66,69,0.2)','rgba(66,66,69,1)');
+      this.mauroLineChart = new RawValuesLineChart('rgba(66,66,69,0.2)', 'rgba(66,66,69,1)');
       this.mauroLineChart.lineChartLabels = [];
       this.mauroLineChart.lineChartData = [
         {label: 'Raw tick data per hour', data: []}
       ];
       if (posts != null) {
         const length = posts.results.length;
-        for (let i = 12; i > 0; i--) {
+        for (let i = 12; i > 1; i--) {
           this.mauroLineChart.lineChartData[0].data.push(posts.results[length - i].ticks);
           this.mauroLineChart.lineChartLabels.push(posts.results[length - i].hour);
         }
@@ -62,17 +62,16 @@ class RawValuesLineChart implements LineChart {
   lineChartLegend: any = true;
   lineChartType: any = 'line';
 
-  constructor(backgroundColor: any, pointColor: any)
-  {
+  constructor(backgroundColor: any, pointColor: any) {
     this.lineChartColors = [
-    {
-      backgroundColor: backgroundColor,
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: pointColor,
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }];
+      {
+        backgroundColor: backgroundColor,
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: pointColor,
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+      }];
   }
 
   chartClicked(e: any): void {
