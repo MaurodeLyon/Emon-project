7bd23d14d878d1ed5efe03195395056a9a33126e
Mauro de Lyon <maurodelyon@gmail.com>
2017-05-02 22:10:04 +0200

    update

--- angularApplication/src/app/services/posts.service.ts
+++ angularApplication/src/app/services/posts.service.ts
@@ -9,18 +9,18 @@ export class PostsService {
   }
 
   getMauroMeasurements() {
-    return this.http.get('http://192.168.1.63:8080/api/website/Mauro').map(res => res.json());
+    return this.http.get('http://84.105.252.233:8080/api/website/Mauro').map(res => res.json());
   }
 
   getArthurMeasurements() {
-    return this.http.get('http://192.168.1.63:8080/api/website/Arthur').map(res => res.json());
+    return this.http.get('http://84.105.252.233:8080/api/website/Arthur').map(res => res.json());
   }
 
   getMauroDelta() {
-    return this.http.get('http://192.168.1.63:8080/api/website/deltaMauro').map(res => res.json());
+    return this.http.get('http://84.105.252.233:8080/api/website/deltaMauro').map(res => res.json());
   }
 
   getArthurDelta() {
-    return this.http.get('http://192.168.1.63:8080/api/website/deltaArthur').map(res => res.json());
+    return this.http.get('http://84.105.252.233:8080/api/website/deltaArthur').map(res => res.json());
   }
 }
